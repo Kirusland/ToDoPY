bbfca37186911580adbb5792d3db6e4e8d06552b
Kirill <matytsyn7@gmail.com>
2024-06-16 01:12:30 +0300

    Added a checker of the exitstence of TODOs.txt

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+
 choice = input('What do you want to do? N/L (New/Load): ')
 if choice == 'N':
     file = open('TODOs.txt', 'a')
     data = input('Enter your TODO: ')
-    file.write('\n'+data)
+    if os.path.exists('TODOs.txt') and os.path.getsize('TODOs.txt') > 0:
+        file.write('\n' + data)
+    else:
+        file.write(data)
+
 elif choice == 'L':
     file = open('TODOs.txt', 'r')
     print(file.read())
