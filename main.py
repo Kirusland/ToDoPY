f23286d7c350ead6a5149b4972bf299a39ef913d
Kirill <matytsyn7@gmail.com>
2024-06-16 01:26:04 +0300

    Made a function of deleting a todo

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
 
+
 def add_todo(data):
     with open('TODOs.txt', 'a') as file:
         num_todos = sum(1 for _ in file)
         file.write(f'\n{num_todos + 1}. {data}')
 
+
+def delete_todo(number):
+    todos = []
+    with open('TODOs.txt', 'r') as file:
+        for line in file:
+            if not line.strip().startswith(f'{number}.'):
+                todos.append(line)
+
+    with open('TODOs.txt', 'w') as file:
+        file.write(''.join(todos))
+
+
 def load_todos():
     with open('TODOs.txt', 'r') as file:
         return file.read()
 
-choice = input('What do you want to do? N/L (New/Load): ')
+
+choice = input('What do you want to do? N/L/D (New/Load/Delete): ')
 if choice == 'N':
     data = input('Enter your TODO: ')
     add_todo(data)
@@ -19,5 +33,9 @@ elif choice == 'L':
         print(todos)
     else:
         print('No TODOs found.')
+elif choice == 'D':
+    number = input('Enter the number of the TODO you want to delete: ')
+    delete_todo(number)
+    print('TODO deleted successfully.')
 else:
     print('Invalid choice')
